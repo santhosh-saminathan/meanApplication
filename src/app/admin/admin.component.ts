019b75bb8b1e4cd24a055f1867064dd8ad837625
santhosh <santhosh.developer@yahoo.com>
2018-03-08 21:33:29 +0530

    UI changes

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -4,62 +4,77 @@ import { AdminService } from './../services/admin.service';
 import { EventService } from './../services/events.service';
 
 @Component({
-  selector: 'admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+    selector: 'admin',
+    templateUrl: './admin.component.html',
+    styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-    newEvents:any = [];
-    allUsers:any = [];
-    allEvents:any = [];
-   
-    constructor(private router: Router,private adminService:AdminService,private eventService:EventService) { }
+    newEvents: any = [];
+    allUsers: any = [];
+    allEvents: any = [];
+
+    constructor(private router: Router, private adminService: AdminService, private eventService: EventService) { }
 
     ngOnInit() {
         console.log("calls automatically");
 
-        this.adminService.getNewEvents().subscribe(data=>{
-            console.log(data);
-            this.newEvents = data;
-        },err=>{
+        this.adminService.getNewEvents().subscribe(data => {
+            if (data) {
+                console.log(data);
+                this.newEvents = data;
+            }
+
+        }, err => {
             console.log(err);
         })
 
-        this.adminService.getAllUsers().subscribe(data=>{
-            console.log(data);
-            this.allUsers = data;
-        },err=>{
+        this.adminService.getAllUsers().subscribe(data => {
+            if (data) {
+                console.log(data);
+                this.allUsers = data;
+            }
+
+        }, err => {
             console.log(err);
         })
 
-        this.eventService.getAllEvents().subscribe(data=>{
-            this.allEvents = data;
-            console.log(this.allEvents);
-        },err=>{
-            console.log(err);
-        })
-    }
+        this.eventService.getAllEvents().subscribe(data => {
+            if (data) {
+                this.allEvents = data;
+                console.log(this.allEvents);
+            }
 
-    approveEvent(eventId){
-        this.adminService.approveEvent({'eventId':eventId}).subscribe(data=>{
-            console.log(data);
-        },err=>{
-            console.log(err);
-        })
-    }
-
-    deleteUser(userId){
-        this.adminService.deleteUser({'userId':userId}).subscribe(data=>{
-            console.log(data);
-        },err=>{
+        }, err => {
             console.log(err);
         })
     }
 
-    deleteEvent(eventId){
-        this.eventService.removeEvent({'eventId':eventId}).subscribe(data=>{
-            this.allEvents = data;
-        },err=>{
+    approveEvent(eventId) {
+        this.adminService.approveEvent({ 'eventId': eventId }).subscribe(data => {
+            if (data) {
+                this.ngOnInit()
+                console.log(data);
+            }
+
+        }, err => {
+            console.log(err);
+        })
+    }
+
+    deleteUser(userId) {
+        this.adminService.deleteUser({ 'userId': userId }).subscribe(data => {
+            console.log(data);
+            this.ngOnInit()
+        }, err => {
+            console.log(err);
+        })
+    }
+
+    deleteEvent(eventId) {
+        this.eventService.removeEvent({ 'eventId': eventId }).subscribe(data => {
+            //this.allEvents = data;
+            this.ngOnInit()
+        }, err => {
             console.log(err);
         })
     }
